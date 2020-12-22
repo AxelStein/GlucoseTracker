e7d692a6b7c72b5843e351f24212887904a444c3
Axel Stein <diefaust1993@gmail.com>
2020-12-23 00:00:32 +0200

    Small fixes
    - Refactor log DAO query
    - Unnecessary settings layout removed

--- app/src/main/java/com/axel_stein/glucose_tracker/data/room/dao/LogDao.kt
+++ app/src/main/java/com/axel_stein/glucose_tracker/data/room/dao/LogDao.kt
@@ -7,9 +7,14 @@ import com.axel_stein.glucose_tracker.data.model.LogItem
 
 @Dao
 interface LogDao {
-    // "select *, food_log.id, 2 as item_type, null as value_mmol, null as value_mg, null as measured, null as note, group_concat(title, \"; \"), date_time from food_log " +
+    // "select *, food_log.id, 2 as item_type, null as value_mmol, null as value_mg, null as measured,
+    // null as note, group_concat(title, \"; \"), date_time from food_log " +
     // "join food_list on food_log.food_id = food_list.id group by food_log.id "
 
-    @Query("select id, value_mmol, value_mg, measured, date_time, 0 as item_type, null as note, null as foods from glucose_log union select id, null as value_mmol, null as value_mg, null as measured, date_time, 1 as item_type, note, null as foods from note_log")
+    @Query("""
+        select id, value_mmol, value_mg, measured, date_time, 0 as item_type, null as note, null as foods from glucose_log union 
+        select id, null as value_mmol, null as value_mg, null as measured, date_time, 1 as item_type, note, null as foods 
+        from note_log
+    """)
     fun getItems(): DataSource.Factory<Int, LogItem>
 }
