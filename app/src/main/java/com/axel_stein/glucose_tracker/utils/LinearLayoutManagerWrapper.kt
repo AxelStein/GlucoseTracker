ef8777c7ecbc7b30de683dc40b1e4cdb964cd762
Axel Stein <diefaust1993@gmail.com>
2021-01-08 00:06:13 +0200

    Add view binding (#13)
    - Log list and archive fragments refactoring
    - Statistics fragment refactoring
    - Main activity view binding

--- app/src/main/java/com/axel_stein/glucose_tracker/utils/LinearLayoutManagerWrapper.kt
+++ app/src/main/java/com/axel_stein/glucose_tracker/utils/LinearLayoutManagerWrapper.kt
@@ -4,7 +4,8 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.recyclerview.widget.LinearLayoutManager
 
-
+// Bug fix for IndexOutOfBoundsException is disabling predictive animations
+// https://stackoverflow.com/questions/31759171/recyclerview-and-java-lang-indexoutofboundsexception-inconsistency-detected-in
 class LinearLayoutManagerWrapper : LinearLayoutManager {
     constructor(context: Context?) : super(context)
     constructor(context: Context?, orientation: Int, reverseLayout: Boolean) : super(context, orientation, reverseLayout)
