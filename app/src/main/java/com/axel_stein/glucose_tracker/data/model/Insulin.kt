77d6579480b1aaa06c8ef931764013e5e94b9314
Axel Stein <diefaust1993@gmail.com>
2021-01-12 15:30:11 +0200

    Update insulin model

--- app/src/main/java/com/axel_stein/glucose_tracker/data/model/Insulin.kt
+++ app/src/main/java/com/axel_stein/glucose_tracker/data/model/Insulin.kt
@@ -7,9 +7,6 @@ import androidx.room.PrimaryKey
 data class Insulin(
     val title: String,
     val type: Int = 0,  // rapid, short, intermediate, long
-    val onset: Int = -1,  // min
-    val peak: Int = -1,  // min
-    val duration: Int = -1  // min
 ) {
     @PrimaryKey(autoGenerate = true)
     var id = 0L
