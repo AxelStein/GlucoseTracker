ab7f6ee4fae7decfc8dc9747318fb06fa20b478a
Axel Stein <diefaust1993@gmail.com>
2021-01-14 15:11:36 +0200

    Update weight activity layout

--- app/src/main/java/com/axel_stein/glucose_tracker/data/model/WeightLog.kt
+++ app/src/main/java/com/axel_stein/glucose_tracker/data/model/WeightLog.kt
@@ -1,9 +1,14 @@
 package com.axel_stein.glucose_tracker.data.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import org.joda.time.DateTime
 
 @Entity(tableName = "weight_log")
 data class WeightLog(
-    val valueKg: Int = 0,
-    val valuePounds: Int = 0
+    var valueKg: Int = 0,
+    var valuePounds: Int = 0,
+
+    @ColumnInfo(name = "date_time")
+    var dateTime: DateTime
 )
